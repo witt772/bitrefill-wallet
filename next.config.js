a05b3c0e5f82b2b9d8fcee5ed413b6d24afb2f28
witt772 <134894729+witt772@users.noreply.github.com>
2026-05-22 10:16:01 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      path: false,
-    };
+  webpack: (config, { isServer }) => {
+    // 开启 Webpack 5 对 WebAssembly 的原生支持
+    config.experiments = { ...config.experiments, asyncWebAssembly: true };
+
+    // 针对服务端渲染的特殊处理，防止在 Node.js 端找不到 WASM 而报错
+    if (isServer) {
+      config.output.webassemblyModuleFilename = './../static/wasm/[modulehash].wasm';
+    } else {
+      config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
+    }
     return config;
   },
 };
